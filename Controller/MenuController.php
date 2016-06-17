4d8da1bc738dcda19de2c7a449d2ca1325b2a52e
Martin Kirilov <wucdbm@gmail.com>
2016-06-17 23:31:48 +0300

    multiget

--- Controller/MenuController.php
+++ Controller/MenuController.php
@@ -3,10 +3,43 @@
 namespace Wucdbm\Bundle\MenuBuilderApiBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
+use Wucdbm\Bundle\MenuBuilderBundle\Entity\Menu;
 use Wucdbm\Bundle\MenuBuilderBundle\Entity\MenuItem;
 use Wucdbm\Bundle\WucdbmBundle\Controller\BaseController;
 
 class MenuController extends BaseController {
+
+    public function multiGetAction(Request $request) {
+        if ($request->query->get('secret') != $this->container->getParameter('wucdbm_menu_builder_api.secret')) {
+            return $this->json([]);
+        }
+
+        $ids = $request->query->get('ids');
+
+        if (!is_array($ids)) {
+            return $this->json([]);
+        }
+
+        $manager = $this->container->get('wucdbm_menu_builder.manager.menus');
+
+        $menus = [];
+
+        foreach ($ids as $id) {
+            $menu = $manager->findOneById($id);
+            if ($menu) {
+                $menus[] = $menu;
+            }
+        }
+
+        $data = [];
+
+        /** @var Menu $menu */
+        foreach ($menus as $menu) {
+            $data[$menu->getId()] = $this->fetchMenu($menu);
+        }
+
+        return $this->json($data);
+    }
 
     public function getAction($id, Request $request) {
         if ($request->query->get('secret') != $this->container->getParameter('wucdbm_menu_builder_api.secret')) {
@@ -25,6 +58,12 @@ class MenuController extends BaseController {
             return $this->json([]);
         }
 
+        $data = $this->fetchMenu($menu);
+
+        return $this->json($data);
+    }
+
+    protected function fetchMenu(Menu $menu) {
         $data = [
             'name' => $menu->getName()
         ];
@@ -37,7 +76,7 @@ class MenuController extends BaseController {
             $data['items'][] = $this->fetchItem($item);
         }
 
-        return $this->json($data);
+        return $data;
     }
 
     protected function fetchItem(MenuItem $item) {
